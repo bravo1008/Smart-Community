7bcd30944eea6df5747459feb120c3f3773862fc
bravo1008 <132344931+bravo1008@users.noreply.github.com>
2024-10-08 13:19:18 +0800

    test-0

---
+++ miniprogram/pages/home/home.ts
@@ -0,0 +1,71 @@
+// pages/home/home.ts
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    motto:'智慧社区'
+  },
+
+  onLoginTap: function() {
+    wx.navigateTo({
+      url: '/pages/index/index' // 假设有一个登录页面
+    });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
