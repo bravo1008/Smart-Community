a486a4b63efaef4ee702730cc7497c75a4d31875
unfair-g <2814315943@qq.com>
2024-10-20 10:21:17 +0800

    新增个人界面

---
+++ miniprogram/pages/personalCenter/personalCenter.ts
@@ -0,0 +1,66 @@
+// pages/personalCenter/personalCenter.ts
+Page({
+
+  data: {
+    avatar: '/assets/dear.jpg', // 默认头像地址
+    nickname: '用户昵称',
+    phone: '123-456-7890',
+    community: '所在社区',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
